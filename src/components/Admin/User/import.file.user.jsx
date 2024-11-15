fb8c9d1ff6abb4835b8754695ca9b70cae50c65c
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-11-15 21:59:43 +0700

    read file excel and display data in it

--- src/components/Admin/User/import.file.user.jsx
+++ src/components/Admin/User/import.file.user.jsx
@@ -1,6 +1,10 @@
 import { InboxOutlined } from "@ant-design/icons";
 import { message, Modal, Table } from "antd";
 import Dragger from "antd/es/upload/Dragger"
+import { useState } from "react";
+import { render } from "react-dom";
+import * as XLSX from 'xlsx'
+
 
 const dummyRequest = ({ file, onSuccess }) => {
     setTimeout(() => {
@@ -8,34 +12,58 @@ const dummyRequest = ({ file, onSuccess }) => {
     }, 1000);
 };
 
-const propsUpload = {
-    name: 'file',
-    multiple: false,
-    maxCount: 1,
-    accept: ".csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",//accept these files type
 
-    // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',->don't need to upload because only need to read file
-    customRequest: dummyRequest,//fake call api
-
-    onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files);
-    },
-};
 
 const ImportUser = (props) => {
 
     const { isImportDataUser, setIsImportDataUser } = props
+
+    const [dataExcel, setDataExcel] = useState([])
+
+    const propsUpload = {
+        name: 'file',
+        multiple: false,
+        maxCount: 1,
+        accept: ".csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",//accept these files type
+
+        // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',->don't need to upload because only need to read file
+        customRequest: dummyRequest,//fake call api
+
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+
+                if (info.fileList && info.fileList.length > 0) {
+                    const file = info.fileList[0].originFileObj
+                    const reader = new FileReader();
+                    reader.readAsArrayBuffer(file);
+
+                    reader.onload = event => {
+                        const data = new Uint8Array(event.target.result)
+                        const workbook = XLSX.read(data, { type: 'array' })
+                        const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+                        const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+                            header: ['fullName', 'email', 'phone'],
+                            range: 1
+                        })
+                        if (jsonData && jsonData.length) {
+                            setDataExcel(jsonData)
+                        }
+                    }
+
+                }
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        },
+    };
 
     return (
         <Modal
@@ -47,7 +75,7 @@ const ImportUser = (props) => {
             okButtonProps={{
                 disabled: true
             }}
-
+            maskClosable={false}
             okText="Import data"
             width={'40vw'}>
             <Dragger {...propsUpload}>
@@ -63,6 +91,7 @@ const ImportUser = (props) => {
             <div style={{ paddingTop: 20 }}>
                 <Table
                     title={() => <span>Data Upload:</span>}
+                    dataSource={dataExcel}
                     columns={[
                         {
                             title: 'Full name',
